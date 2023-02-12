180eeecc0088f6665604041aede72740d1e3dbf4
Augustin Bost <augustin.bost@epitech.eu>
2023-02-12 10:36:57 +0100

    FEAT scroll down with ICONS, UPDATE Home.js

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Class_img } from '../components/class';
 import Drugs from '../components/Drugs';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
@@ -7,6 +8,7 @@ const Home = () => {
     return (
         <div>
             <Header />
+            <Class_img />
             <Drugs title="Maux de tête" n1="Dolipranne 1000mg" n2="Dolipranne 1000mg"
             n3="Dolipranne 1000mg" n4="Dolipranne 1000mg" n5="Dolipranne 1000mg" s1="./doliprane.png"
             s2="./doliprane.png" s3="./doliprane.png" s4="./doliprane.png" s5="./doliprane.png" />
